a96ddee3ebe55c7924948d66c3a783df5458b64e
Chirantan Choudhuri <chirantanchoudhuri@Chirantans-MacBook-Air.local>
2025-09-11 09:09:32 +0530

    Hotel Reservation Application with local storage

---
+++ hotel-reservation-system/src/app/reservation/reservation.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeModule } from '../home/home.module';
+
+
+
+@NgModule({
+  declarations: [
+    ReservationFormComponent,
+    ReservationListComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HomeModule
+  ]
+})
+export class ReservationModule { }
