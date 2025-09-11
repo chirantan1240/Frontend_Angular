6eba62c6d7ce9aa4bff9f0c28cc5703a502df8c1
Chirantan Choudhuri <chirantanchoudhuri@Chirantans-MacBook-Air.local>
2025-09-11 20:39:06 +0530

    Handle Hotel Reservations with mockupAPI(mockoon)

---
+++ apiHotel-reservation/api-hotelReservation/src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from "../app-routing.module";
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+],
+  exports : [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
